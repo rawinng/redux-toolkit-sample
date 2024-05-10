2c8a956ac45b75a405bc2b34fa6e8ffb819815b1
Rawin Ngamloet <rawin.ngamloet@scb.co.th>
2024-05-10 08:47:02 +0700

    Init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello, world!!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders logo", () => {
+  render(<App />);
+  const logoElement = screen.getByAltText(/logo/i);
+  expect(logoElement).toBeInTheDocument();
+});
+
+test("renders header", () => {
+  render(<App />);
+  // get by class name
+  const headerElement = screen.getByTestId("header");
+  expect(headerElement).toBeInTheDocument();
 });
